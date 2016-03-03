57d27860377b4ee837378624d4481dad9052afc5
J.T. Gilkeson <jt@gogobot.com>
2016-03-02 22:38:03 -0800

    Cleaner days of month calculation

--- library/src/main/java/com/andexert/calendarlistview/library/CalendarUtils.java
+++ library/src/main/java/com/andexert/calendarlistview/library/CalendarUtils.java
@@ -24,29 +24,12 @@
 package com.andexert.calendarlistview.library;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class CalendarUtils
 {
 	public static int getDaysInMonth(int month, int year) {
-        switch (month) {
-            case Calendar.JANUARY:
-            case Calendar.MARCH:
-            case Calendar.MAY:
-            case Calendar.JULY:
-            case Calendar.AUGUST:
-            case Calendar.OCTOBER:
-            case Calendar.DECEMBER:
-                return 31;
-            case Calendar.APRIL:
-            case Calendar.JUNE:
-            case Calendar.SEPTEMBER:
-            case Calendar.NOVEMBER:
-                return 30;
-            case Calendar.FEBRUARY:
-                return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ? 29 : 28;
-            default:
-                throw new IllegalArgumentException("Invalid Month");
-        }
+        return new GregorianCalendar(year, month, 1).getActualMaximum(Calendar.DAY_OF_MONTH);
 	}
 }
